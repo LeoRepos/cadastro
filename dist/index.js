b74fb0cfc53c3eb868898f89768f9ea93d4d6d19
Leonardo Dos Santos Dias <dleonardo@adhosting.internal>
2023-01-23 09:32:06 -0300

    fix/ Refatorando pastas e adicionando Typescript no projeto

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict"
+//* Inferência de Tipos
+let myStr = "Minha String";
+let myInt = 10;
+let myFloat = 10.5; 
+let myDate = new Date(); 
+let myArray = [1, 2, 3];
+let myObj = { name: 'nome', value: 5 };
